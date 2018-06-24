1c4144176ab1196f2f82e19b5b54121f3e7d6573
David Parra Perez <dparra0007@users.noreply.github.com>
2018-06-24 23:43:26 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
-export ROUND_DBURL="jdbc:postgresql://round-db/rounddb"
-export ROUND_DBUSER="rounddbuser"
+export ROUND_DBURL="jdbc:postgresql://course-db/coursedb"
+export ROUND_DBUSER="coursedbuser"
 export ROUND_DBPASS="pass"
