1f6def582c3d8c957359737800af206c2743a31b
David Parra Perez <dparra0007@users.noreply.github.com>
2018-07-02 13:12:51 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export OPENTRACING_BASEURL=http://trace-global-w53-trace.192.168.64.53.nip.io/zipkin/
-export DISCOVERY_BASEURL=http://discovery-global-w53-discovery.192.168.64.53.nip.io/eureka
+export OPENTRACING_BASEURL=http://trace-global-w53-trace.dit-iaggs.com
+export DISCOVERY_BASEURL=http://greetingapi-discovery:8761/eureka
