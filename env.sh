93f3fa9390867f878f92b1babafe026e80becaff
David Parra Perez <dparra0007@users.noreply.github.com>
2018-07-02 13:14:01 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export OPENTRACING_BASEURL=http://trace-global-w53-trace.dit-iaggs.com
+export OPENTRACING_BASEURL=http://trace-global-w53-trace.192.168.64.53.nip.io
 export DISCOVERY_BASEURL=http://greetingapi-discovery:8761/eureka
