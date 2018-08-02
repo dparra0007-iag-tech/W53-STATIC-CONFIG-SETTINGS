1f73475e9663e37e22122a3e1e41d4c72eaa4cf4
David Parra Perez <dparra0007@users.noreply.github.com>
2018-08-02 12:21:05 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export OPENTRACING_BASEURL=http://trace-global-ocp01-trace-stg.apps.ocp01.hcp.iairgroup.com
-export DISCOVERY_BASEURL=http://discovery-global-w53-discovery.dit-iaggs.com/eureka
+export DISCOVERY_BASEURL=http://discovery-global-ocp01-discovery-stg.apps.ocp01.hcp.iairgroup.com/eureka
