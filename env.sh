73a983d809d74115f5f13cf805b7cf3dc54cafa2
David Parra Perez <dparra0007@users.noreply.github.com>
2018-11-19 09:53:42 +0100

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 export MEMBERSHIP_DBURL="jdbc:postgresql://membership-db/membershipdb"
 export MEMBERSHIP_DBUSER="membershipdbuser"
 export MEMBERSHIP_DBPASS="pass"
-export MEMBERSHIP_SENTENCES_URL="http://gturnquist-quoters.cfapps.io/api/random"
+export MEMBERSHIP_SENTENCES_URL="http://proxy:8080/api/random"
