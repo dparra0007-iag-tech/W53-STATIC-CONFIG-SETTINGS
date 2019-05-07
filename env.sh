f1f90d838e7182c33c625a6ea4c578e8c1f493af
David Parra Perez <dparra0007@users.noreply.github.com>
2019-05-08 00:10:23 +0200

    Update env.sh
    update new endpoints

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export OPENTRACING_BASEURL=http://trace-global-trace.dit-iaggs.com
-export DISCOVERY_BASEURL=http://discovery-global-discovery.dit-iaggs.com/eureka
+export OPENTRACING_BASEURL=http://trace-global-trace-global.dit-iaggs.com
+export DISCOVERY_BASEURL=http://discovery-global-discovery-global.dit-iaggs.com/eureka
