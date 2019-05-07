9972d92fd6c5154a04fed339ffc42731d2810a74
David Parra Perez <dparra0007@users.noreply.github.com>
2019-05-07 16:07:47 +0200

    Update env.sh
    change stg and production endpoints

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export OPENTRACING_BASEURL=http://trace-global-w53-trace.dit-iaggs.com
-export DISCOVERY_BASEURL=http://discovery-global-w53-discovery.dit-iaggs.com/eureka
+export OPENTRACING_BASEURL=http://trace-global-ocp01-trace-stg.apps.ocp01.hcp.iairgroup.com
+export DISCOVERY_BASEURL=http://discovery-global-ocp01-discovery-stg.apps.ocp01.hcp.iairgroup.com/eureka
