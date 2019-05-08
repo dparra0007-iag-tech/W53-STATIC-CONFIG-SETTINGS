bf0fba2762d6dee16f088762240be691337e9168
David Parra Perez <dparra0007@users.noreply.github.com>
2019-05-08 18:47:22 +0200

    Create karate-config.js
    change pro endpoint to pro ocp

---
+++ karate-config.js
@@ -0,0 +1,18 @@
+function fn() {    
+  var env = karate.env; // get system property 'karate.env'
+  karate.log('karate.env system property was:', env);
+  if (!env) {
+    env = 'dev';
+  }
+  var config = {
+    env: env,
+	  baseUrl: 'http://greetingapi-apigateway-ocp01-greetingapi-stg.apps.ocp01.hcp.iairgroup.com/greetingapi'
+  }
+  if (env == 'dev') {
+    // customize
+    // e.g. config.foo = 'bar';
+  } else if (env == 'e2e') {
+    // customize
+  }
+  return config;
+}
